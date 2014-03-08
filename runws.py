e0a6be02d294e6bcd22482ba93c1db7a45a19226
sneshyba <nesh@pugetsound.edu>
2014-03-07 18:05:33 -0800

    unbundled function from application
    weibull_whole_image was unbundled into wstuff.py (functions) and
    runws.py

--- runws.py
+++ runws.py
@@ -9,28 +9,6 @@ import numpy as np
 import wstuff as ws
 from scipy.optimize import leastsq
 
-def getHistogram2(filebase, bins, rangemax, limits):
-    ''' Filebase is the name of the folder, where the CSVs are.
-        Num is the number of CSVs that are to be parsed and put together.
-        Bins is the number of bins to use when making a histogram. Returns
-        spacing (labels), hist (values), data (all r values in one list).'''
-
-    # Getting all r values from file
-    data, zfixed, xgrid, ygrid = ws.getR(filebase, limits)
-
-    # Creating the histogram
-    print data
-    print bins
-    
-    hist, rhist = np.histogram(data, bins=bins, range=(0, rangemax),
-                               density=True)
-
-    # Making our own spacing - middle of all intervals.
-    spacing = rhist[1:]-(rhist[1]-rhist[0])/2
-
-    return spacing, hist, data, zfixed, xgrid, ygrid 
-
-
 # -- BEGIN PARAMETERS ----
 bins = 25
 rangemax = 0.25
@@ -43,20 +21,23 @@ register_path = 'C:\ice_register.csv'
 # ------ END PARAMETERS --
 
 # Get histogram for set number of bins
-labels, values, data, zfixed, xgrid, ygrid = getHistogram2(namebase, bins, rangemax, limits)
-
+#labels, values, data, zfixed, xgrid, ygrid = getHistogram2(namebase, bins, rangemax, limits)
+data, zfixed, xgrid, ygrid = ws.getR(namebase, limits)
+values, rhist = np.histogram(data, bins=bins, range=(0, rangemax),density=True)
+    
 # If there are gaps, find the maximum possible number of bins to not get them
 while not (values > 0).all():
     bins = bins-1
-    labels, values, data, zfixed, xgrid, ygrid = getHistogram2(namebase, bins, rangemax, limits)
+    values, rhist = np.histogram(data, bins=bins, range=(0, rangemax),density=True)
     if (bins == 5):
         print('WARNING: Ideal bins are lower than 5, printing them anyway.')
         break
 
+# Get the spacing (labels) and do some reporting
+labels = rhist[1:]-(rhist[1]-rhist[0])/2
+total_mean = np.mean(data)
 print("Number of bins determined as: " + str(bins))
 print("Completed histogram from "+str(data.size)+" r values.")
-
-total_mean = np.mean(data)
 print("The total mean is: "+str(total_mean)+".")
 
 # Setting estimated values for sigma and eta
@@ -91,11 +72,8 @@ ax1.set_xlabel(r"$r$")
 # Setting up functions to convert r <---> phi
 r_to_phi = lambda x: 180*np.arccos(1-x)/np.pi
 phi_to_r = lambda x: 1 - np.cos((x*np.pi)/180)
-
 new_tick_locations = phi_to_r(np.array([5, 10, 15, 20, 25, 30, 35, 40]))
-
 tick_function = lambda x: ["%.0f" % z for z in r_to_phi(x)]
-
 ax2.set_xticks(new_tick_locations)
 ax2.set_xticklabels(tick_function(new_tick_locations))
 ax2.set_xlabel(r"$\phi$")
@@ -116,10 +94,7 @@ plt.text(0.84, 0.77,
          horizontalalignment='center',
          verticalalignment='center',
          transform=ax1.transAxes)
-
 plt.show()
-
-
 
 # Saving figure to disk
 plt.savefig(namebase+'weib_comp.png', dpi=100)
@@ -136,7 +111,6 @@ plt.close(fignum)
 Nx, Ny = np.shape(zfixed)
 i = int(Nx/2)
 j = int(Ny/2)
-
 x1=i-20; x2=i+20; y1=j-20; y2=j+20
 ax = plt.figure(fignum).gca(projection='3d') # Set up a three dimensional graphics window 
 ax.plot_surface(xgrid[y1:y2,x1:x2],ygrid[y1:y2,x1:x2],zfixed[y1:y2,x1:x2],rstride=1,cstride=1) # Make the mesh plot
@@ -148,9 +122,6 @@ plt.show()
 
 # Saving figure to disk
 plt.savefig(namebase+'surface.png', dpi=200)
-
-
-
 
 #plt.pcolor(xgrid[y1:y2,x1:x2],ygrid[y1:y2,x1:x2],zfixed[y1:y2,x1:x2])
 #plt.colorbar()
